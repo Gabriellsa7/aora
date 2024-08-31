3339819e164280c5781da088b8714eff379be4fc
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-31 09:20:20 -0300

    feat: created a global text component

---
+++ src/components/text/text.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text, TextStyle, StyleSheet } from "react-native";
+
+interface TextComponentProps {
+  children: React.ReactNode;
+  style?: TextStyle; // Allows you to add custom styles
+  weight?: "regular" | "bold"; // Example of style variation
+  size?: number; // font size
+  color?: string; // text color
+}
+
+const TextComponent: React.FC<TextComponentProps> = ({
+  children,
+  style,
+  weight = "regular",
+  size = 14,
+  color = "#000",
+}) => {
+  return (
+    <Text
+      style={[
+        styles.text,
+        {
+          fontWeight: weight === "bold" ? "bold" : "normal",
+          fontSize: size,
+          color: color,
+        },
+        style, // Allows you to add custom styles
+      ]}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    //choose some font
+    fontFamily: "System",
+  },
+});
+
+export default TextComponent;
