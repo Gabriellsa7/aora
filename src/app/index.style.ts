fb228481c802706860dead2fa0442755a77f9c36
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-31 09:19:30 -0300

    feat: created the index style and start the first screen

---
+++ src/app/index.style.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#161622",
+    justifyContent: "center",
+    paddingLeft: 21,
+    paddingRight: 21,
+  },
+  section: {
+    flex: 1,
+    alignItems: "center",
+    gap: 25,
+  },
+  text: {
+    textAlign: "center",
+  },
+  textContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
---
+++ src/app/index.tsx
@@ -0,0 +1,42 @@
+import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import Logo from "../components/logo/logo";
+import TextComponent from "../components/text/text";
+import Button from "../components/button/button";
+import { styles } from "./index.style";
+
+const statusBarHeight = Constants.statusBarHeight;
+
+export default function Index() {
+  return (
+    <>
+      <StatusBar translucent backgroundColor="#161622" barStyle="default" />
+      <View style={[styles.container, { paddingTop: statusBarHeight + 8 }]}>
+        <View style={[styles.section, { paddingTop: statusBarHeight + 40 }]}>
+          <Logo />
+          <View>
+            <Image
+              source={require("../../assets/initial-page-images/initial-banner.png")}
+            />
+          </View>
+          <View style={styles.textContainer}>
+            <TextComponent
+              size={30}
+              weight="bold"
+              color="#FFFFFF"
+              style={styles.text}
+            >
+              Discover Endless Possibilities with Aora
+            </TextComponent>
+            <TextComponent size={14} style={styles.text} color="#FFFFFF">
+              Where Creativity Meets Innovation: Embark on a Journey of
+              Limitless Exploration with Aora
+            </TextComponent>
+          </View>
+          <Button title="Login" textStyle={styles.buttonText} />
+          {/* <Button title="Sign up" textStyle={styles.buttonText} /> */}
+        </View>
+      </View>
+    </>
+  );
+}
