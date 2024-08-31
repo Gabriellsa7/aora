fc45fa694aa937418276667bc2d23fc13f5f1614
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-31 09:20:37 -0300

    feat: start the login page

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "./login.styles";
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
