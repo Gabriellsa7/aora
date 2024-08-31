be14609dfd57ab9e3abf2f856b578b141a8ce944
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-31 16:56:16 -0300

    feat: created register page

---
+++ src/app/pages/sign-up/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "./register.styles";
+
+const Register = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Register</Text>
+    </View>
+  );
+};
+
+export default Register;
