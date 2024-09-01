5190e817fdc9f7dd9916ad9c8f0f8b83b0fec0b6
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-01 00:05:34 -0300

    style: finish sign up style

--- src/app/pages/sign-up/register.styles.ts
+++ src/app/pages/sign-up/register.styles.ts
@@ -3,7 +3,23 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    width: "100%",
+    gap: 40,
+    paddingHorizontal: 24,
+    backgroundColor: "#161622",
+  },
+  inputContainer: {
+    gap: 22,
+    paddingVertical: 32,
+  },
+  passwordText: {
+    alignItems: "flex-end",
+  },
+  button: {
+    width: "100%",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
