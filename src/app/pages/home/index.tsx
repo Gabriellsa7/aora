cddf77550a6fe564011b730fda3bfa37405cb7fa
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-01 00:04:05 -0300

    feat: created home page

---
+++ src/app/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "./home.style";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
