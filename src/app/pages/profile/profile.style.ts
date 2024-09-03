88cae0e8bb7c01685fa6f85a5f27429ea1400d69
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-03 19:03:08 -0300

    feat: created profile style

---
+++ src/app/pages/profile/profile.style.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    gap: 20,
+    backgroundColor: "#161622",
+  },
+  profileContainer: {
+    alignItems: "center",
+    gap: 12,
+  },
+});
