ca2dce56067af87ad05b1008a9d371bd96ad9d2e
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-03 15:03:09 -0300

    feat: created the bottom tab with every components in routes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,46 @@
 import { Slot } from "expo-router";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Created from "./pages/created";
+import Saved from "./pages/saved";
+import { FontAwesome5 } from "@expo/vector-icons";
+import Login from "./pages/login";
+
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: any;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "user" : "user";
+            } else if (route.name === "Created") {
+              iconName = focused ? "plus-circle" : "plus-circle";
+            } else {
+              iconName = focused ? "bookmark" : "bookmark";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Created" component={Created} />
+        <Tab.Screen name="Saved" component={Saved} />
+      </Tab.Navigator>
+      {/* <Slot />; */}
+    </NavigationContainer>
+  );
 }
