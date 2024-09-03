a90cfa1b5b37750a8f05c8be7c294aa76919d7ca
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-03 19:02:46 -0300

    feat: start to create profile screen

--- src/app/pages/profile/index.tsx
+++ src/app/pages/profile/index.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import Constants from "expo-constants";
+import { styles } from "./profile.style";
+import { MaterialIcons } from "@expo/vector-icons";
+import TextComponent from "@/src/components/text/text";
+
+const statusBarHeight = Constants.statusBarHeight;
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={[styles.container, { paddingTop: statusBarHeight + 8 }]}>
+      <View style={{ alignItems: "flex-end" }}>
+        <MaterialIcons name="logout" size={24} color={"red"} />
+      </View>
+      <View style={styles.profileContainer}>
+        <Image source={require("@/assets/avatar.png")} />
+        <TextComponent color="#FFFFFF" size={18} weight="bold">
+          Boruto
+        </TextComponent>
+      </View>
     </View>
   );
 };
