29af6fbcb47841323dfa81408a77e196143bb48a
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-03 15:03:27 -0300

    feat: created the Created component

---
+++ src/app/pages/created/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Created = () => {
+  return (
+    <View>
+      <Text>Created</Text>
+    </View>
+  );
+};
+
+export default Created;
