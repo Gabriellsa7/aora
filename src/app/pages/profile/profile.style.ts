5f0ff4930f71f7b80cc99307d0a0a306d3fe86d6
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-06 16:22:02 -0300

    style: added styles to profile page

--- src/app/pages/profile/profile.style.ts
+++ src/app/pages/profile/profile.style.ts
@@ -3,6 +3,9 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
     paddingHorizontal: 24,
     gap: 20,
     backgroundColor: "#161622",
@@ -10,5 +13,11 @@ export const styles = StyleSheet.create({
   profileContainer: {
     alignItems: "center",
     gap: 12,
+  },
+  postsViewsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 23,
   },
 });
