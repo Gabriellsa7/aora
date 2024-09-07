3db47a4b681313ee0d340816a6a1afb171681ec2
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-07 03:53:37 -0300

    style: added styles to created component

---
+++ src/app/pages/created/created.style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: 24,
+    gap: 20,
+    backgroundColor: "#161622",
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
