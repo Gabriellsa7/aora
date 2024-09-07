1cfe2afd1f69dce722d81f3bfa5e4415e4862fac
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-07 03:53:19 -0300

    feat: created the Created component

--- src/app/pages/created/index.tsx
+++ src/app/pages/created/index.tsx
@@ -1,11 +1,68 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import Constants from "expo-constants";
+import { styles } from "./created.style";
+import TextComponent from "@/src/components/text/text";
+import Input from "@/src/components/input/input";
+import Button from "@/src/components/button/button-component";
+
+const statusBarHeight = Constants.statusBarHeight;
 
 const Created = () => {
+  const [videoTitle, setVideoTitle] = useState("");
   return (
-    <View>
-      <Text>Created</Text>
-    </View>
+    <>
+      {/* <ScrollView style={{ backgroundColor: "#161622", width: "100%" }}> */}
+      <View style={[styles.container, { paddingTop: statusBarHeight + 50 }]}>
+        <View>
+          <TextComponent color="#FFFFFF" weight="bold" size={24}>
+            Upload Videos
+          </TextComponent>
+        </View>
+        <View>
+          <Input
+            value={videoTitle}
+            placeholder="Give your video a catchy title..."
+            placeholderTextColor={"#7B7B8B"}
+            onChangeText={setVideoTitle}
+          />
+          <Input
+            label="Upload Video"
+            value={videoTitle}
+            style={{
+              width: "100%",
+              height: 119,
+              backgroundColor: "#1E1E2D",
+              borderRadius: 8,
+              padding: 10,
+            }}
+            placeholder="Give your video a catchy title..."
+            placeholderTextColor={"#7B7B8B"}
+            onChangeText={setVideoTitle}
+          />
+          <Input
+            label="Thumbnail Image"
+            value={videoTitle}
+            placeholder="Choose a file"
+            placeholderTextColor={"#7B7B8B"}
+            onChangeText={setVideoTitle}
+          />
+          <Input
+            label="AI Prompt"
+            value={videoTitle}
+            placeholder="Give your video a catchy title..."
+            placeholderTextColor={"#7B7B8B"}
+            onChangeText={setVideoTitle}
+          />
+        </View>
+        <Button
+          title="Submit & Publish"
+          style={{ width: "100%" }}
+          textStyle={styles.buttonText}
+        />
+      </View>
+      {/* </ScrollView> */}
+    </>
   );
 };
 
