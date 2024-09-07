fda90cffedec3aaaa0ce9fa51f4a4dbadbac2f33
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-09-07 03:31:17 -0300

    style: added styles to saved component

---
+++ src/app/pages/saved/saved.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    width: "100%",
+    paddingHorizontal: 24,
+    gap: 20,
+    backgroundColor: "#161622",
+  },
+});
